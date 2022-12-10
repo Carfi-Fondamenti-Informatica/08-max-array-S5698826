aa63b7eeee929e1348c765939c3b3d919d4dbed3
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-10 17:27:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
 int main(){
+    int N;
+    cin>>N;
+    float numeri[N];
+    for(int i=0;i<N;i++){
+        cin>>numeri[i];
+    }
+    cout<<array(numeri,N)<<endl;
+    return 0;
   return 0;
 }
